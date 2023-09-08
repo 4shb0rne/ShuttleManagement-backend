cac37ecd9b53bd0d166c48b467b392ee7c3f3072
anggaraswn <anggara11swn@gmail.com>
2023-09-08 16:46:54 +0700

    addCORS

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('dotenv').config();
+var createError = require("http-errors");
+var express = require("express");
+const cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
 
-
-const bodyParser = require('body-parser');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var scheduleRouter = require('./routes/schedule');
-var individualRF = require('./routes/individualRF');
-var groupRF = require('./routes/groupRF');
+const bodyParser = require("body-parser");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var scheduleRouter = require("./routes/schedule");
+var individualRF = require("./routes/individualRF");
+var groupRF = require("./routes/groupRF");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(cors());
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/schedules', scheduleRouter);
-app.use('/registration', individualRF);
-app.use('/group-registration', groupRF);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/schedules", scheduleRouter);
+app.use("/registration", individualRF);
+app.use("/group-registration", groupRF);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    res.send("respond with a resource");
 });
 
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const { User } = require('../models');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { User } = require("../models");
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
         const newUser = await User.create({
             username,
-            password: hashedPassword
+            password: hashedPassword,
         });
 
         res.status(201).json(newUser);
@@ -26,19 +26,21 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
     const user = await User.findOne({ where: { username } });
-    if (!user) return res.status(404).json({ error: 'User not found' });
+    if (!user) return res.status(404).json({ error: "User not found" });
 
-    if (!await bcrypt.compare(password, user.password)) {
-        return res.status(403).json({ error: 'Incorrect password' });
+    if (!(await bcrypt.compare(password, user.password))) {
+        return res.status(403).json({ error: "Incorrect password" });
     }
 
-    const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET);
+    const accessToken = jwt.sign(
+        { username: user.username },
+        process.env.ACCESS_TOKEN_SECRET
+    );
     res.json({ accessToken });
 });
 
 module.exports = router;
-
