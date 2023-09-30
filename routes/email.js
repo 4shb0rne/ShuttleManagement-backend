7ea19a6c5bd56663923ce14b4c8aa9fb5d471a49
anggaraswn <anggara11swn@gmail.com>
2023-09-30 13:32:56 +0700

    groupRegistration

--- routes/email.js
+++ routes/email.js
@@ -3,13 +3,13 @@ var router = express.Router();
 const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_EMAIL,
-    pass: process.env.SMTP_PASSWORD,
-  },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+    },
 });
 
 router.post("/send", (req, res) => {
@@ -42,13 +42,81 @@ router.post("/send", (req, res) => {
         from: process.env.SMTP_EMAIL,
         to: req.body.to,
         subject: req.body.subject,
-        html: emailContent
+        html: emailContent,
     };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-        res.status(500).send(error);
+            res.status(500).send(error);
         } else {
-        res.status(200).send("Email sent: " + info.response);
+            res.status(200).send("Email sent: " + info.response);
+        }
+    });
+});
+
+router.post("/sendGroup", (req, res) => {
+    const binusianIDs = req.body.binusianID.split(",");
+    const names = req.body.name.split(",");
+
+    let tableRows = "";
+
+    for (let i = 0; i < binusianIDs.length; i++) {
+        tableRows += `
+            <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">${i + 1}</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${
+                binusianIDs[i]
+            }</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${names[i]}</td>
+    </tr>
+        `;
+    }
+
+    let emailContent = `
+    <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Email Confirmation</title>
+        </head>
+        <body style="font-family: Arial, sans-serif;">
+            <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #ddd;">
+                <div>
+                    <img src="https://upload.wikimedia.org/wikipedia/id/a/a2/Logo_Binus_University.png" alt="Your Brand Logo" width="200" />
+                </div>
+                <div style="margin-top: 20px;">
+                    <h2>Dear, ${req.body.to}</h2>
+                    <p>Berikut adalah konfirmasi untuk reservasi shuttle pada tanggal <b>${req.body.date}</b> pukul ${req.body.time} dengan data penumpang sebagai berikut :</p>
+                    <p>Urusan     : ${req.body.purpose}</p>
+                    <table style="width: 100%; border-collapse: collapse;">
+                <thead>
+                    <tr>
+                        <th style="border: 1px solid #ddd; padding: 8px;">No.</th>
+                        <th style="border: 1px solid #ddd; padding: 8px;">BinusianID</th>
+                        <th style="border: 1px solid #ddd; padding: 8px;">Nama</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${tableRows}
+                </tbody>
+            </table>
+                    <p style="margin-top: 20px;">Best Regards,</p>
+                    <p>Lecturer Service Center - Binus University</p>
+                </div>
+            </div>
+        </body>
+    </html>
+        `;
+    let mailOptions = {
+        from: process.env.SMTP_EMAIL,
+        to: req.body.to,
+        subject: req.body.subject,
+        html: emailContent,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.status(500).send(error);
+        } else {
+            res.status(200).send("Email sent: " + info.response);
         }
     });
 });
--- routes/individualRF.js
+++ routes/individualRF.js
@@ -4,119 +4,196 @@ var rf = require("../models").RegistrationForm;
 var rfd = require("../models").RegistrationFormDetail;
 var Shuttleschedule = require("../models").ShuttleSchedule;
 const { Op } = require("sequelize");
+const { sequelize } = require("../models");
 
 //GET : Get Registration Datas by Schedule
 router.get("/schedule", async (req, res) => {
-  try {
-    const scheduleID = parseInt(req.query.scheduleID, 10); // Convert to integer
-    const useDate = req.query.useDate;
-    const details = await rfd.findAll({
-      where: {
-        scheduleID: scheduleID,
-      },
-      include: [
-        {
-          model: rf,
-          where: {
-            useDate: {
-              [Op.eq]: useDate,
+    try {
+        const scheduleID = parseInt(req.query.scheduleID, 10); // Convert to integer
+        const useDate = req.query.useDate;
+        const details = await rfd.findAll({
+            where: {
+                scheduleID: scheduleID,
             },
-          },
-        },
-      ],
-    });
-    return res.status(200).json(details);
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "Server error", error: err.message });
-  }
+            include: [
+                {
+                    model: rf,
+                    where: {
+                        useDate: {
+                            [Op.eq]: useDate,
+                        },
+                    },
+                },
+            ],
+        });
+        return res.status(200).json(details);
+    } catch (err) {
+        return res
+            .status(500)
+            .json({ message: "Server error", error: err.message });
+    }
+});
+
+//GET : Get Registration Datas that have status 'waiting'
+router.get("/groupRequest", async (req, res) => {
+    try {
+        const registrations = await rf.findAll({
+            where: {
+                status: "Waiting",
+            },
+            include: [
+                {
+                    model: rfd,
+                    as: "details",
+                    where: {
+                        registrationID: sequelize.col("details.registrationID"),
+                    },
+                    include: [
+                        {
+                            model: Shuttleschedule,
+                            as: "schedulesDetails", // This should match the alias in the ShuttleSchedule model's associations.
+                        },
+                    ],
+                },
+            ],
+        });
+        return res.status(200).json(registrations);
+    } catch (err) {
+        return res
+            .status(500)
+            .json({ message: "Server error", error: err.message });
+    }
+});
+
+router.put("/processRequest", async (req, res) => {
+    const registrationId = req.query.id;
+    const processCode = req.query.processCode;
+
+    if (!registrationId) {
+        return res
+            .status(400)
+            .json({ success: false, message: "ID is required" });
+    }
+
+    try {
+        let updatedRows;
+        console.log(processCode);
+        if (processCode == 1) {
+            updatedRows = await rf.update(
+                { status: "Approved" },
+                { where: { RegistrationID: registrationId } }
+            );
+        } else {
+            updatedRows = await rf.update(
+                { status: "Rejected" },
+                { where: { RegistrationID: registrationId } }
+            );
+        }
+
+        if (updatedRows[0] === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Registration Form not found",
+            });
+        }
+
+        res.status(201).json({
+            success: true,
+            message: "Status updated successfully",
+        });
+    } catch (error) {
+        console.error("Error updating status:", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
 });
 
 //ADD : Add Registration
 router.post("/add", async (req, res) => {
-  try {
-    const {
-      binusianID,
-      name,
-      phoneNumber,
-      email,
-      purpose,
-      useDate,
-      status,
-      scheduleID,
-      scheduleID2,
-    } = req.body;
+    try {
+        const {
+            binusianID,
+            name,
+            phoneNumber,
+            email,
+            purpose,
+            useDate,
+            status,
+            scheduleID,
+            scheduleID2,
+        } = req.body;
 
-    const registration = await rf.create({
-      binusianID,
-      name,
-      phoneNumber,
-      email,
-      purpose,
-      useDate,
-      status,
-    });
-    const newRegistrationID = registration.RegistrationID;
+        const registration = await rf.create({
+            binusianID,
+            name,
+            phoneNumber,
+            email,
+            purpose,
+            useDate,
+            status,
+        });
+        const newRegistrationID = registration.RegistrationID;
 
-    await rfd.create({
-      scheduleID,
-      registrationID: newRegistrationID,
-    });
-    await rfd.create({
-      scheduleID: scheduleID2,
-      registrationID: newRegistrationID,
-    });
-    res.status(201).json({
-      registration,
-      message: `New registration created with ID: ${newRegistrationID}`,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+        await rfd.create({
+            scheduleID,
+            registrationID: newRegistrationID,
+        });
+        await rfd.create({
+            scheduleID: scheduleID2,
+            registrationID: newRegistrationID,
+        });
+        res.status(201).json({
+            registration,
+            message: `New registration created with ID: ${newRegistrationID}`,
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 // DELETE: Delete a registration by ID
 router.delete("/:id", async (req, res) => {
-  try {
-    const deleted = await rf.destroy({
-      where: { RegistrationID: req.params.id },
-    });
-    if (deleted) {
-      res.status(204).send("Registration deleted");
-    } else {
-      res.status(404).json({ error: "Registration not found" });
+    try {
+        const deleted = await rf.destroy({
+            where: { RegistrationID: req.params.id },
+        });
+        if (deleted) {
+            res.status(204).send("Registration deleted");
+        } else {
+            res.status(404).json({ error: "Registration not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 // READ: Get a registration by ID
 router.get("/:id", async (req, res) => {
-  try {
-    const registration = await rf.findByPk(req.params.id, {
-      include: [
-        {
-          model: rfd,
-          as: "details",
-          include: [
-            {
-              model: Shuttleschedule,
-              as: "schedulesDetails", // This should match the alias in the ShuttleSchedule model's associations.
-            },
-          ],
-        },
-      ],
-    });
-    if (registration) {
-      res.json(registration);
-    } else {
-      res.status(404).json({ error: "Registration not found" });
+    try {
+        const registration = await rf.findByPk(req.params.id, {
+            include: [
+                {
+                    model: rfd,
+                    as: "details",
+                    include: [
+                        {
+                            model: Shuttleschedule,
+                            as: "schedulesDetails", // This should match the alias in the ShuttleSchedule model's associations.
+                        },
+                    ],
+                },
+            ],
+        });
+        if (registration) {
+            res.json(registration);
+        } else {
+            res.status(404).json({ error: "Registration not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 module.exports = router;
