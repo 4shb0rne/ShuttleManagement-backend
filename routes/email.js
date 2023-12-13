f6b8d4977e72402fe3a45fe1c32508b266df851e
4shb0rne <william080@binus.ac.id>
2023-12-13 22:31:02 +0700

    try to add qr code to email(1)

--- routes/email.js
+++ routes/email.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const nodemailer = require("nodemailer");
+const QRCode = require("qrcode");
 
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -12,45 +13,60 @@ let transporter = nodemailer.createTransport({
     },
 });
 
-router.post("/send", (req, res) => {
-    let emailContent = `
-    <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="UTF-8">
-            <title>Email Confirmation</title>
-        </head>
-        <body style="font-family: Arial, sans-serif;">
-            <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #ddd;">
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/id/a/a2/Logo_Binus_University.png" alt="Your Brand Logo" width="200" />
+router.post("/send", async(req, res) => {
+    const qrUrl = req.body.url;
+    try {
+        const qrCodeDataURL = await QRCode.toDataURL(qrUrl, {
+            type: 'image/png',
+            margin: 1,
+            width: 200
+        });
+
+        let emailContent = `
+        <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>Email Confirmation</title>
+            </head>
+            <body style="font-family: Arial, sans-serif;">
+                <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #ddd;">
+                    <div>
+                        <img src="https://upload.wikimedia.org/wikipedia/id/a/a2/Logo_Binus_University.png" alt="Your Brand Logo" width="200" />
+                    </div>
+                    <div style="margin-top: 20px;">
+                        <h2>Dear, ${req.body.name}</h2>
+                        <p>Booking Detail : </p>
+                        <img src="${qrCodeDataURL}" alt="QR Code" />
+                        <p>Berikut adalah konfirmasi untuk reservasi shuttle pada tanggal <b>${req.body.date}</b> pukul <b>${req.body.time1}</b> dan <b>${req.body.time2}</b> dengan data penumpang sebagai berikut :</p>
+                        <p>BinusianID : ${req.body.binusianID}</p>
+                        <p>Nama       : ${req.body.name}</p>
+                        <p>Urusan     : ${req.body.purpose}</p>
+                        <p style="margin-top: 20px;">Best Regards,</p>
+                        <p>Lecturer Service Center - Binus University</p>
+                    </div>
                 </div>
-                <div style="margin-top: 20px;">
-                    <h2>Dear, ${req.body.name}</h2>
-                    <p>Berikut adalah konfirmasi untuk reservasi shuttle pada tanggal <b>${req.body.date}</b> pukul <b>${req.body.time1}</b> dan <b>${req.body.time2}</b> dengan data penumpang sebagai berikut :</p>
-                    <p>BinusianID : ${req.body.binusianID}</p>
-                    <p>Nama       : ${req.body.name}</p>
-                    <p>Urusan     : ${req.body.purpose}</p>
-                    <p style="margin-top: 20px;">Best Regards,</p>
-                    <p>Lecturer Service Center - Binus University</p>
-                </div>
-            </div>
-        </body>
-    </html>
+            </body>
+        </html>
         `;
-    let mailOptions = {
-        from: process.env.SMTP_EMAIL,
-        to: req.body.to,
-        subject: req.body.subject,
-        html: emailContent,
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            res.status(500).send(error);
-        } else {
-            res.status(200).send("Email sent: " + info.response);
-        }
-    });
+
+        let mailOptions = {
+            from: process.env.SMTP_EMAIL,
+            to: req.body.to,
+            subject: req.body.subject,
+            html: emailContent,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                res.status(500).send(error);
+            } else {
+                res.status(200).send("Email sent: " + info.response);
+            }
+        });
+    } catch (error) {
+        res.status(500).send('Error generating QR code');
+    }
 });
 
 router.post("/sendGroup", (req, res) => {
