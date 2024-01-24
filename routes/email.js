b313a6832188b2b930c69a41a82a2214df3b0da3
4shb0rne <william080@binus.ac.id>
2024-01-24 22:25:33 +0700

    update email

--- routes/email.js
+++ routes/email.js
@@ -30,21 +30,15 @@ router.post("/send", async(req, res) => {
             </head>
             <body style="font-family: Arial, sans-serif;">
                 <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #ddd;">
-                    <div>
-                        <img src="https://upload.wikimedia.org/wikipedia/id/a/a2/Logo_Binus_University.png" alt="Your Brand Logo" width="200" />
-                    </div>
-                    <div>
-                        <img src="${qrCodeDataURL}" alt="QR Code" style="width: 150px; height: 150px;" />
-                    </div>
-                    <div>
-                        <a href="${url}">Click Here to Verify</a>
-                    </div>
                     <div style="margin-top: 20px;">
                         <h2>Dear, ${req.body.name}</h2>
                         <p>Berikut adalah konfirmasi untuk reservasi shuttle pada tanggal <b>${req.body.date}</b> pukul <b>${req.body.time1}</b> dan <b>${req.body.time2}</b> dengan data penumpang sebagai berikut :</p>
                         <p>BinusianID : ${req.body.binusianID}</p>
                         <p>Nama       : ${req.body.name}</p>
                         <p>Urusan     : ${req.body.purpose}</p>
+                        <div>
+                            <a href="${url}">Click Here to Verify</a>
+                        </div>
                         <p style="margin-top: 20px;">Best Regards,</p>
                         <p>Lecturer Service Center - Binus University</p>
                     </div>
@@ -100,9 +94,6 @@ router.post("/sendGroup", (req, res) => {
         </head>
         <body style="font-family: Arial, sans-serif;">
             <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #ddd;">
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/id/a/a2/Logo_Binus_University.png" alt="Your Brand Logo" width="200" />
-                </div>
                 <div style="margin-top: 20px;">
                     <h2>Dear, ${req.body.to}</h2>
                     <p>Berikut adalah konfirmasi untuk reservasi shuttle pada tanggal <b>${req.body.date}</b> pukul ${req.body.time1} dan pukul ${req.body.time2} dengan data penumpang sebagai berikut :</p>
